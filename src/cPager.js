a850b8a3b73d72ac106ea6ed578f807259f0464d
zhchi-me <zhangchixx8@gmail.com>
2018-01-08 12:34:32 +0800

    commit

--- src/cPager.js
+++ src/cPager.js
@@ -16,57 +16,61 @@
 			nextPageText	: "下一页",
 			gotoBtnText		: "确定"
 		},
+
 		// 绘制 html控件
 		show: function (conf) {
 			if (conf) {
 				this.user(conf);
 			}
 
+			this.pn = this.getPage() ? this.getPage() : this.pn;
+
 			var btn_first = '', btn_pre = '', btn_next = '', btn_last = '', pgBtn = '', pgBtndiv = '', pgInput = '';
 			if (this.showFirstPage) {	//显示首页按钮
-				btn_first = this.pn == 1 ? '<a class="disable">'+ this.config.firstPageText +'</a>' : '<a class="pgBtn">'+ this.config.firstPageText +'</a>';
+				btn_first = this.pn == 1 ? '<a class="disable">'+ this.config.firstPageText +'</a>' : '<a class="pgBtn" onclick="$.cPager.gotoPage(1)">'+ this.config.firstPageText +'</a>';
 			}
 			if (this.showPrePage) {		//显示上一页按钮
-				btn_pre = this.pn == 1 ? '<a class="disable">'+ this.config.prePageText +'</a>' : '<a class="pgBtn">'+ this.config.prePageText +'</a>';
+				btn_pre = this.pn == 1 ? '<a class="disable">'+ this.config.prePageText +'</a>' : '<a class="pgBtn" onclick="$.cPager.gotoPage('+ (this.pn - 1) +')">'+ this.config.prePageText +'</a>';
 			}
 			if (this.showLastPage) {	//显示尾页按钮
-				btn_last = this.pn == this.total ? '<a class="disable">'+ this.config.lastPageText +'</a>': '<a class="pgBtn">'+ this.config.lastPageText +'</a>';
+				btn_last = this.pn == this.total ? '<a class="disable">'+ this.config.lastPageText +'</a>': '<a class="pgBtn" onclick="$.cPager.gotoPage('+ this.total +')">'+ this.config.lastPageText +'</a>';
 			}
 			if (this.showNextPage) {	//显示下一页按钮
-				btn_next = this.pn == this.total ? '<a class="disable">'+ this.config.nextPageText +'</a>' : '<a class="pgBtn">'+ this.config.nextPageText +'</a>';
+				var nextNum = parseInt(this.pn) + 1;
+				btn_next = this.pn == this.total ? '<a class="disable">'+ this.config.nextPageText +'</a>' : '<a class="pgBtn" onclick="$.cPager.gotoPage('+ nextNum +')">'+ this.config.nextPageText +'</a>';
 			}
 
 			if (this.total <= this.showMax) {		// 总页码小于显示Max
 				for (var i = 1; i <= this.total; i++) {
 					if (i == this.pn) {
-						pgBtn += '<a class="active">'+ i +'</a>'; 
+						actBtn();
 					} else {
-						pgBtn += '<a class="pgBtn">'+ i +'</a>'; 
+						pgb();
 					}
 				}
 			} else {								// 总页码大于显示Max
 				if (this.pn <= this.showMax / 2 + 1) {
 					for (var i = 1; i <= this.showMax; i++) {
 						if (i == this.pn) {
-							pgBtn += '<a class="active">'+ i +'</a>'; 
+							actBtn();
 						} else {
-							pgBtn += '<a class="pgBtn">'+ i +'</a>'; 
+							pgb();
 						}
 					}
-					pgBtn += '<a class="active">···</a>'; 
+					ellipsis();
 				} else {
 					if (this.pn > this.total - parseInt(this.showMax / 2) - 1) {
-						pgBtn += '<a class="active">···</a>';
+						ellipsis();
 						var p = this.total - this.showMax + 1;
 						for (var i = p; i <= this.total; i++) {
 							if (i == this.pn) {
-								pgBtn += '<a class="active">'+ i +'</a>'; 
+								actBtn();
 							} else {
-								pgBtn += '<a class="pgBtn">'+ i +'</a>'; 
+								pgb();
 							}
 						}
 					} else {
-						pgBtn += '<a class="active">···</a>';
+						ellipsis();
 						if (this.showMax % 2) {
 							var p = this.pn - parseInt(this.showMax / 2);
 						} else {
@@ -74,40 +78,86 @@
 						}
 						for (var i = p; i < this.showMax + p; i++) {
 							if (i == this.pn) {
-								pgBtn += '<a class="active">'+ i +'</a>'; 
+								actBtn();
 							} else {
-								pgBtn += '<a class="pgBtn">'+ i +'</a>'; 
+								pgb();
 							}
 						}
-						pgBtn += '<a class="active">···</a>';
+						ellipsis();
 					}
 				}
+			}
+
+			function pgb() {
+				pgBtn += '<a class="pgBtn" onclick="$.cPager.gotoPage('+ i +')">'+ i +'</a>'; 
+			}
+
+			function actBtn() {
+				pgBtn += '<a class="active">'+ i +'</a>'; 
+			}
+			
+			function ellipsis() {
+				pgBtn += '<a class="active">···</a>';
 			}
 
 			pgBtndiv += '<div id="pgBtndiv">'+ btn_first + btn_pre + pgBtn + btn_next + btn_last +'</div>';
 			$('#cPager').append(pgBtndiv);
 
 			if (this.showGotoPage) {
-				pgInput += '<div id="pgInput"> 共'+ this.total +'页，'+' 跳到第 <input type="text" id="gotopage"> 页'+
-							' <button id="gotoBtn" onclick="$.cPager.gotopage()">'+ $.cPager.config.gotoBtnText +'</button></div>';
+				pgInput += '<div id="pgInput"> 共'+ this.total +'页，'+' 跳到第 <input type="text" id="gotoPage"> 页'+
+							' <button id="gotoBtn" onclick="$.cPager.gotoPage()">'+ $.cPager.config.gotoBtnText +'</button></div>';
 				$('#cPager').append(pgInput);
 			}
 		},
+
 		// 用户设置
 		user: function (conf) {
 			this.pn = conf.pn ? conf.pn : this.pn;
 			this.total = conf.total ? conf.total : this.total;
 			this.showMax = conf.showMax ? conf.showMax : this.showMax;
-			this.showFirstPage = conf.showFirstPage != undefined ? conf.showFirstPage : this.showFirstPage;
-			this.showLastPage = conf.showLastPage != undefined ? conf.showLastPage : this.showLastPage;
-			this.showPrePage = conf.showPrePage != undefined ? conf.showPrePage : this.showPrePage;
-			this.showNextPage = conf.showNextPage != undefined ? conf.showNextPage : this.showNextPage;
-			this.showGotoPage = conf.showGotoPage != undefined ? conf.showGotoPage : this.showGotoPage;
+			if (conf.showFirstPage != undefined) this.showFirstPage = conf.showFirstPage;
+			if (conf.showLastPage != undefined) this.showLastPage = conf.showLastPage;
+			if (conf.showPrePage != undefined) this.showPrePage = conf.showPrePage;
+			if (conf.showNextPage != undefined) this.showNextPage = conf.showNextPage;
+			if (conf.showGotoPage != undefined) this.showGotoPage = conf.showGotoPage;
 		},
+
 		// 页面跳转
-		gotopage: function () {
-			var pgNum = $("#gotopage").val();
-			console.log(pgNum);
+		gotoPage: function (pn) {
+			if (pn) {
+				window.location.href = this.getUrl(pn);
+			} else {
+				var pgNum = $("#gotoPage").val();
+				if (pgNum) {
+					window.location.href = this.getUrl(pgNum);
+				}
+			}
+		},
+
+		getPage: function () {
+			var reg = /(^|&)pn=([^&]*)($|&)/;
+			var pn = window.location.search.substr(1).match(reg);
+			if (pn) {
+				pn = pn[2];
+				return pn;
+			} else if (!pn) {
+				return null;
+			}
+		},
+
+		getUrl: function (pn) {
+			var localUrl = window.location.href;
+			if (localUrl.indexOf('?') == -1) {
+				localUrl += "?pn=" + pn;
+			} else {
+				if (localUrl.indexOf('pn') == -1) {
+					localUrl += "&pn=" + pn;
+				} else {
+					localUrl = localUrl.substr(0, localUrl.lastIndexOf('pn=')) + "pn=" + pn + localUrl.substr(localUrl.lastIndexOf('pn=') + this.getPage().length + 3);
+				}
+			}
+			return localUrl;
 		}
+
 	}
 })(jQuery);
